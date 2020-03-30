c46d059af6d8a741f7aa5a3a3e0e784025c78600
Annabelle Zhang <qiaoyan1@ualberta.ca>
2020-03-30 17:26:40 -0600

    verify server input

--- server.c
+++ server.c
@@ -11,14 +11,29 @@
 #include <string.h>
 #include <sys/types.h>
 #include <time.h> 
+#include <stdbool.h>
+#include <ctype.h> 
+
+#define MIN_PORT_NUM 5000
+#define MAX_PORT_NUM 64000
+
+bool isNum(char* num);
+int getPortNum(int argc, char *argv[]);
 
 int main(int argc, char *argv[])
 {
     int listenfd = 0, connfd = 0;
+    int port;
     struct sockaddr_in serv_addr; 
 
     char sendBuff[1025];
     time_t ticks; 
+
+    if ((port = getPortNum(argc, argv)) == -1) {
+        return -1;
+    };
+
+    printf("port: %d\n", port);
 
     listenfd = socket(AF_INET, SOCK_STREAM, 0);
     memset(&serv_addr, '0', sizeof(serv_addr));
@@ -26,7 +41,7 @@ int main(int argc, char *argv[])
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv_addr.sin_port = htons(5000); 
+    serv_addr.sin_port = htons(port); 
 
     bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)); 
 
@@ -43,4 +58,37 @@ int main(int argc, char *argv[])
         close(connfd);
         sleep(1);
      }
+}
+
+bool isNum(char* num) {
+    for (int i = 0; i < strlen(num); i++){
+        if (isdigit(num[i]) == 0) {
+            return false; 
+        }
+    }
+    return true; 
+} 
+
+// verify the input and return port number
+int getPortNum(int argc, char *argv[]) {
+    int port;
+
+    if(argc != 2) 
+    {
+        printf("\n Usage: %s <port number> \n", argv[0]);
+        return -1;
+    } else
+    {
+        if (!isNum(argv[1])) {
+            printf("\n Usage: %s <port number> \n", argv[0]);
+            return -1;
+        } 
+        port = atoi(argv[1]);
+        if ((port < MIN_PORT_NUM) || (port > MAX_PORT_NUM))
+        {
+            printf("The port must be in the range %d to %d.\n", MIN_PORT_NUM, MAX_PORT_NUM);
+            return -1;
+        }
+    }
+    return port;
 }
